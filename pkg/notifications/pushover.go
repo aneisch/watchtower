5360b6d72c18cdaf7fc7b14973be2ff2e0767dba
Andrew <andrewneisch@gmail.com>
2020-05-26 01:32:56 -0500

    Create pushover.go

---
+++ pkg/notifications/pushover.go
@@ -0,0 +1,36 @@
+package notifications
+
+import (
+	t "github.com/containrrr/watchtower/pkg/types"
+	"github.com/johntdyer/slackrus"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+  "github.com/gregdel/pushover"
+)
+
+const (
+	slackType = "pushover"
+)
+
+type pushoverTypeNotifier struct {
+	pushover.Pushover
+}
+
+func newPushoverkNotifier(c *cobra.Command, acceptedLogLevels []log.Level) t.Notifier {
+	flags := c.PersistentFlags()
+
+	app, _ := flags.GetString("notification-pushover-app")
+
+	n := &pushoverTypeNotifier{
+		Pushover: pushover.Pushover{
+			token:        app,
+		},
+	}
+
+	log.AddHook(n)
+	return n
+}
+
+func (s *pushoverTypeNotifier) StartNotification() {}
+
+func (s *pushoverTypeNotifier) SendNotification() {}
